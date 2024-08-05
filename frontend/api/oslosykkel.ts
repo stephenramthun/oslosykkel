db1e3d2a48b64b9aa7c6a73d46257d6b9b3e56ac
Stephen Ramthun <stephenramthun@gmail.com>
2024-08-05 21:17:56 +0200

    Use own REST api for frontend

--- frontend/api/oslosykkel.ts
+++ frontend/api/oslosykkel.ts
@@ -1,22 +1,8 @@
-const baseUrl = "https://gbfs.urbansharing.com/oslobysykkel.no"
+const baseUrl = "http://localhost:8000/api/stations"
 const clientIdentifier = "stephenramthun-oslosykkel"
 
-export const getStationData = async (): Promise<StationInformationResponse> => {
-  const response = await fetch(`${baseUrl}/station_information.json`, {
-    headers: {
-      "Client-Identifier": clientIdentifier
-    }
-  })
-
-  if (response.ok) {
-    return response.json()
-  } else {
-    throw response
-  }
-}
-
-export const getStationStatus = async (): Promise<StationStatusResponse> => {
-  const response = await fetch(`${baseUrl}/station_status.json`, {
+export const getStationData = async (): Promise<(StationInformation & StationStatus)[]> => {
+  const response = await fetch(baseUrl, {
     headers: {
       "Client-Identifier": clientIdentifier
     }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,28 +1,8 @@
-import { getStationData, getStationStatus } from "@/api/oslosykkel";
+import { getStationData } from "@/api/oslosykkel";
 import styles from "./page.module.css";
 
-const mergeStationData = (information: StationInformation[], status: StationStatus[]): (StationInformation & StationStatus)[] => {
-  const data: Record<string, Partial<StationInformation & StationStatus>> = {}
-
-  for (const station of information) {
-    data[station.station_id] = station
-  }
-
-  for (const station of status) {
-    data[station.station_id].num_bikes_available = station.num_bikes_available
-    data[station.station_id].num_docks_available = station.num_docks_available
-    data[station.station_id].is_installed = station.is_installed
-    data[station.station_id].is_renting = station.is_renting
-    data[station.station_id].is_returning = station.is_returning
-  }
-
-  return Object.values(data) as (StationInformation & StationStatus)[]
-}
-
 export default async function Home() {
-  const information = await getStationData()
-  const status = await getStationStatus()
-  const stations = mergeStationData(information.data.stations, status.data.stations)
+  const data = await getStationData()
 
   return (
     <main className={styles.main}>
@@ -37,7 +17,7 @@ export default async function Home() {
         </tr>
         </thead>
         <tbody>
-        {stations.map(station => (
+        {Object.values(data).map(station => (
           <tr key={station.station_id}>
             <td>
               {station.name}
